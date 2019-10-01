9130141243fb1f3e192d3dad2fd24183651296da
Tony Ngo <tngo@dms.net>
2019-10-01 13:41:10 -0500

    init listify

---
+++ Listtify/ListifyAPI/Controllers/ListifyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpFishUtil;
+
+namespace ListifyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListifyController : ControllerBase
+    {
+        public ListifyController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get(int start, int end, int index)
+        {
+            var myList = new ListiFy(start, end);
+
+            return Ok(myList[index]);
+        }
+    }
+}
---
+++ Listtify/SpFishUtil/ListiFy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpFishUtil
+{
+    public class ListiFy
+    {
+        private readonly IEnumerable<int> _range;
+
+        public ListiFy()
+        {
+        }
+
+        public ListiFy(int start, int end)
+        {
+            _range = Enumerable.Range(start, end);
+        }
+
+        public int this[int index]
+        {
+            get { return _range.ToArray()[index]; }
+        }
+    }
+}
